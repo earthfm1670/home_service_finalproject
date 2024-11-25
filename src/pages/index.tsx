23cdc98e1e7ca9528268d416af76123d25b0fb10
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-11-25 16:32:17 +0700

    add: develop branch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,6 +35,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>Lets go</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
