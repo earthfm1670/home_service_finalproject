dd87e51569c48fd91a40c11de70bfd5fddd4808d
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-11-25 16:34:05 +0700

    fix: adjust devlop branch main file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,6 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
-          <li>Lets goooo</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
