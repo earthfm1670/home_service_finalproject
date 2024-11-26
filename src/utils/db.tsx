e8bc1f536da7fdc9077dc4190137bff9b6b7a940
Thanapat1502 <psy150201027@gmail.com>
2024-11-26 16:14:23 +0700

    feat: add error handling for supabaseUrl and supabaseKey

--- src/utils/db.tsx
+++ src/utils/db.tsx
@@ -1,11 +1,22 @@
 import { createClient } from "@supabase/supabase-js";
-import "dotenv/config";
 import { Pool } from "pg";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl) {
+  throw new Error("Supabase URL is missing");
+}
+if (!supabaseKey) {
+  throw new Error("Supabase Key is missing");
+}
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
+const connectionString = process.env.CONNECTION_STRING;
+if (!connectionString) {
+  throw new Error("connectionString is missing");
+}
 export const connectionPool = new Pool({
-  connectionString: process.env.CONNECTION_STRING,
+  connectionString: connectionString,
 });
