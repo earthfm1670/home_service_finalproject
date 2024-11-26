88c3603c9d6054b2498b199a5889825293eaf639
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-11-26 15:02:21 +0700

    add: initial styling to hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,23 @@ export default function Home() {
     <>
       <Navbar />
       <div className="h-[704px] bg-blue-100">
-        <div>
-          <h1>เรื่องบ้าน...ให้เราช่วยดูแลคุณ</h1>
-          <h2>"สะดวก ราคาคุ้มค่า เชื่อถือได้"</h2>
-          <p>ซ่อมเครื่องใช้ไฟฟ้า ซ่อมแอร์</p>
-          <p>ทำความสะอาดบ้าน</p>
-          <p>โดยพนักงานแม่บ้าน และช่างมืออาชีพ</p>
-          <button>เช็คราคาบริการ</button>
+        <div className="h-[345px] w-full">
+          <h1 className="w-full text-[40px] text-blue-700">
+            เรื่องบ้าน...ให้เราช่วยดูแลคุณ
+          </h1>
+          <h2 className="w-full text-[20px] text-black">
+            "สะดวก ราคาคุ้มค่า เชื่อถือได้"
+          </h2>
+          <p className="w-full text-[18px] text-gray-700">
+            ซ่อมเครื่องใช้ไฟฟ้า ซ่อมแอร์
+          </p>
+          <p className="w-full text-[18px] text-gray-700">ทำความสะอาดบ้าน</p>
+          <p className="w-full text-[18px] text-gray-700">
+            โดยพนักงานแม่บ้าน และช่างมืออาชีพ
+          </p>
+          <button className="w-1/2 py-3 px-6 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-500 focus:bg-blue-800">
+            เช็คราคาบริการ
+          </button>
         </div>
       </div>
     </>
