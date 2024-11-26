c3ac425dbe1566e8f77c0ab6472416008609b661
Thanapat1502 <psy150201027@gmail.com>
2024-11-26 13:28:23 +0700

    feat: create user login api

---
+++ src/pages/api/auth/login.ts
@@ -0,0 +1,32 @@
+import { supabase } from "@/utils/db";
+import { NextApiRequest, NextApiResponse } from "next";
+
+interface UserLoginRequestBody {
+  email: string;
+  password: string;
+}
+
+export default async function userLogin(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { email, password }: UserLoginRequestBody = req.body;
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (error) {
+    //check if credential invalid
+    if (
+      error.code === "invalid_credential" ||
+      error.message.includes("Invalid login credential")
+    ) {
+      return res.status(400).json({ error: "Your password is incorect" });
+    }
+    return res.status(400).json({ error: error.message });
+  }
+  return res.status(200).json({
+    message: "Signed in successfully",
+    access_token: data.session.access_token,
+  });
+}
--- src/utils/db.tsx
+++ src/utils/db.tsx
@@ -1,5 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 import "dotenv/config";
+import { connection } from "next/server";
+import * as pg from "pg";
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 export const supabase = createClient(supabaseUrl, supabaseKey);
+const { Pool } = pg.default;
+export const connectionPool = new Pool({
+  connectionString: process.env.CONNECTION_STRING,
+});
