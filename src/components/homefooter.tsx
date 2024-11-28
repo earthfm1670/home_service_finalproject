fbf448e327f5067f066ae4ecd1bfdadb595666ad
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-11-29 00:26:38 +0700

    add: complete footer top section styling

--- src/components/homefooter.tsx
+++ src/components/homefooter.tsx
@@ -7,7 +7,7 @@ export default function HomeFooter() {
       <footer>
         {/* top footer section */}
         <div className="w-full h-[273px]">
-          <div className="flex items-center">
+          <div className="flex items-center m-4">
             <img
               src="/image/footerhouse.svg"
               className="w-[39px] h-[39px]"
@@ -16,29 +16,37 @@ export default function HomeFooter() {
               HomeServices
             </div>
           </div>
-          <div>
-            <h2 className="text-[18px] font-medium text-gray-950">
+          <div className="my-4">
+            <h2 className="text-[18px] font-medium text-gray-950 mx-4 my-2">
               บริษัท โฮมเซอร์วิสเซส จำกัด
             </h2>
-            <p className="text-[14px] font-normal text-gray-800">
+            <p className="text-[14px] font-normal text-gray-800 mx-4">
               452 ซอยสุขุมวิท 79 แขวงพระโขนงเหนือ เขตวัฒนา
             </p>
-            <p className="text-[14px] font-normal text-gray-800">
+            <p className="text-[14px] font-normal text-gray-800 mx-4">
               กรุงเทพมหานคร 10260
             </p>
           </div>
           <div className="">
             <div className="flex items-center">
-              <div>
-                <img src="/image/phoneicon.svg" alt="phoneicon"></img>
+              <div className="w-[20px] h-[20px] mr-2 mx-4">
+                <img
+                  src="/image/phoneicon.svg"
+                  alt="phoneicon"
+                  className="w-full h-full"
+                ></img>
               </div>
               <div className="text-[16px] font-normal text-gray-800">
                 080-540-6357
               </div>
             </div>
             <div className="flex items-center">
-              <div>
-                <img src="/image/mailicon.svg" alt="mailicon"></img>
+              <div className="w-[20px] h-[20px] mr-2 mx-4">
+                <img
+                  src="/image/mailicon.svg"
+                  alt="mailicon"
+                  className="w-full h-full"
+                ></img>
               </div>
               <div className="text-[16px] font-normal text-gray-800">
                 contact@homeservices.co
