d60d4bcb0f802c7b7f7ed14ab2cd956eb52e47c3
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-11-29 09:05:02 +0700

    edit: overall margin/padding of footer top section

--- src/components/homefooter.tsx
+++ src/components/homefooter.tsx
@@ -6,8 +6,8 @@ export default function HomeFooter() {
       {/* Footer Container */}
       <footer>
         {/* top footer section */}
-        <div className="w-full h-[273px] lg:px-44 py-4 lg:h-[151px] lg:flex lg:flex-row lg:items-start">
-          <div className="flex items-center m-4 lg:my-6">
+        <div className="w-full h-[273px] lg:px-44 lg:h-[151px] lg:flex lg:flex-row lg:items-start">
+          <div className="flex items-center m-4 my-6 lg:my-6">
             <img
               src="/image/footerhouse.svg"
               className="w-[39px] h-[39px]"
@@ -16,7 +16,7 @@ export default function HomeFooter() {
               HomeServices
             </div>
           </div>
-          <div className="my-4 lg:mx-8">
+          <div className="my-6 lg:mx-8">
             <h2 className="text-[18px] font-medium text-gray-950 mx-4 my-2">
               บริษัท โฮมเซอร์วิสเซส จำกัด
             </h2>
@@ -26,13 +26,13 @@ export default function HomeFooter() {
             <p className="text-[14px] font-normal text-gray-800 mx-4 lg:hidden">
               กรุงเทพมหานคร 10260
             </p>
-            <p className="text-[14px] font-normal text-gray-800 mx-4 lg:block">
+            <p className="text-[14px] font-normal text-gray-800 mx-4 hidden lg:block">
               452 ซอยสุขุมวิท 79 แขวงพระโขนงเหนือ เขตวัฒนา กรุงเทพมหานคร 10260
             </p>
           </div>
           <div className="lg:my-6">
             <div className="flex items-center lg:pt-2">
-              <div className="w-[20px] h-[20px] mr-2 mx-4">
+              <div className="w-[20px] h-[20px] mr-2 mx-4 my-2">
                 <img
                   src="/image/phoneicon.svg"
                   alt="phoneicon"
@@ -70,6 +70,7 @@ export default function HomeFooter() {
           <div className="text-[12px] text-gray-500 font-normal mx-4 lg:hidden">
             copyright © 2021 HomeServices.com All rights reserved
           </div>
+          {/* footer bottom for desktop */}
           <div className="lg:w-full lg:flex lg:items-center lg:justify-around">
             <div className="text-[12px] text-gray-500 font-normal mx-4 hidden lg:block">
               copyright © 2021 HomeServices.com All rights reserved
