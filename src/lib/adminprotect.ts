ac96c9f8e09c4504c057e0c064f47663c90ce157
Thanapat1502 <psy150201027@gmail.com>
2024-12-02 16:59:02 +0700

    fix: middleware on hold.

--- src/lib/adminprotect.ts
+++ src/lib/adminprotect.ts
@@ -1,9 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import jwt from "jsonwebtoken";
 export function protectAdmin(req: NextApiRequest, res: NextApiResponse) {
   const token = req.headers.authorization;
   if (!token || !token.startsWith("Bearer ")) {
     return res.status(403).json({ error: "Token has invalid format" });
   }
   const tokenWithoutBearer = token.split(" ")[1];
-  //jwt.verify(tokenWitoutBearer, porcess.env.SECRET_KEY, (error, payload))
+
+  // jwt.verify(tokenWithoutBearer, process.env.SECRET_KEY, (err, payload))=>{
+  //   if(err){
+  //     return res.status(401).json({message: "Token is invalid"})
+  //   }
+  //   req.user = payload;
+  // };
+
 }
