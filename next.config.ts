c3b0e1450fb4a6468ad80d7ac9ffe13d8916a00c
Wannasingh <sarankhtn@gmail.com>
2024-12-02 11:55:21 +0700

    feat: add images config in nextjs to improve image quality

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "frqdeijtcguxcozmpucc.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/service_pictures/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
