493991a68fce4a7e79715a7d5318cb5c17e44021
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-12-02 13:51:53 +0700

    add: cursor pointer

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -22,20 +22,20 @@ export function Navbar() {
       className="flex justify-between items-center border shadow-lg p-2 px-4 bg-white lg:px-32 "
       onClick={redirectToHome}
     >
-      <div className="flex gap-10">
+      <div className="flex gap-10 cursor-pointer">
         <img src="/image/homeservicelogo.svg"></img>
 
         <div className="hidden lg:block" onClick={redirectToServiceList}>
           บริการของเรา
         </div>
       </div>
-      <div className="flex items-center gap-2">
-        <div className="lg:hidden" onClick={redirectToServiceList}>
+      <div className="flex items-center gap-2 cursor-pointer">
+        <div className="lg:hidden " onClick={redirectToServiceList}>
           บริการของเรา
         </div>
         <div
           onClick={redirectToLogin}
-          className="flex justify-center items-center w-[90px] h-[37px] text-[14px] font-medium text-blue-600 bg-white border border-blue-600 px-2 hover:text-blue-400 hover:border-blue-400 rounded-lg"
+          className="flex justify-center items-center w-[90px] h-[37px] text-[14px] font-medium text-blue-600 bg-white border border-blue-600 px-2 hover:text-blue-400 hover:border-blue-400 rounded-lg cursor-pointer"
         >
           <button>เข้าสู่ระบบ</button>
         </div>
