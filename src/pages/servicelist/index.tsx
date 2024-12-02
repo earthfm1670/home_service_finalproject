ef6cbf8c12a84faeb0312e7d57ca8b435dfcf65d
Kittisak Saengsri <kittisaksa77@gmail.com>
2024-12-02 11:59:09 +0700

    feat: create servicescontext for dependency injection and use it in index.tsx to provide services to components.

--- src/pages/servicelist/index.tsx
+++ src/pages/servicelist/index.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import ServicesListBannerFooter from "@/components/ServicesListBannerFooter";
 import ServicesListRendering from "@/components/ServicesListRendering";
 import ServicesListFilteredData from "@/components/ServicesListFilteredData";
+import { ServicesProvider } from "@/components/ServicesContext";
 
 const ServicesList: React.FC = () => {
   return (
-    <>
+    <ServicesProvider>
       <ServicesListFilteredData />
       <ServicesListRendering />
       <ServicesListBannerFooter />
-    </>
+    </ServicesProvider>
   );
 };
 
