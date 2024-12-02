663049439a904eebeb98b157220678e745c79f0a
Thanapat1502 <psy150201027@gmail.com>
2024-12-02 10:33:14 +0700

    fix: update supabase.tsx with admin key

--- src/utils/supabase.tsx
+++ src/utils/supabase.tsx
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const admininKey = process.env.SUPABASE_SERVICE_ROLE;
 
 if (!supabaseUrl) {
   throw new Error("Supabase URL is missing");
@@ -8,5 +9,9 @@ if (!supabaseUrl) {
 if (!supabaseKey) {
   throw new Error("Supabase Key is missing");
 }
+if (!admininKey) {
+  throw new Error("Admin Key is missing");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+export const adminSupabase = createClient(supabaseUrl, admininKey);
