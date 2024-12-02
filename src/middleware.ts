1a29b0ad3cd31a75b90e5d817a1882ce13ade9ce
Thanapat1502 <psy150201027@gmail.com>
2024-12-02 16:59:02 +0700

    fix: middleware on hold.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
 
-type MiddlewareMap = {
-  [key: string]: (req: NextRequest) => NextResponse<unknown>;
-};
-
-// const middlewareMap: MiddlewareMap = {
-//   ""
-// }
-
-export async function middleware(req: NextRequest, res: NextResponse) {
-  console.log("MW Testing");
-  console.log(req.method);
-  console.log(req.url);
-  console.log(req.headers.get("origin"));
-
-  if (req.method === "GET") {
-    return new NextResponse(JSON.stringify({ mwm: "MW Tsting" }), {
-      status: 200,
-    });
-  }
-  // Logic
-  return NextResponse.next();
+export function middleware(req: NextRequest) {
+  if (!req.headers.authorization)
+    return NextResponse.redirect(new URL(`/`, req.url));
 }
 
-export const config = { matcher: ["/api/admin/management/edit"] };
+// type MiddlewareMap = {
+//   [key: string]: (req: NextRequest) => NextResponse<unknown>;
+// };
+// const middlewareMap: MiddlewareMap = {
+//   "/api/admin": protectAdmin,
+// };
+
+// export async function middleware(req: NextRequest) {
+//   // console.log("MW Testing");
+//   // console.log(req.method);
+//   // console.log(req.url);
+//   // console.log(req.headers.get("origin"));
+//   try {
+//     for (const path in middlewareMap) {
+//       if (req.nextUrl.pathname.startsWith(path)) {
+//         const handler = middlewareMap[path];
+//         if (handler) {
+//           return handler(req);
+//         }
+//       }
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+export const config = { matcher: ["/api/admin/"] };
