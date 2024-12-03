5f42e3e729cecd56c34789bd4723761d766676f1
Thanapat1502 <psy150201027@gmail.com>
2024-12-03 10:09:32 +0700

    feature: basic edit api for admin, in-progress

--- src/pages/api/admin/management/edit.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { connectionPool } from "@/utils/db";
-
-export default async function edit(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ m: "This is from edit" });
-}
---
+++ src/pages/api/admin/management/edit/[editId].ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { connectionPool } from "@/utils/db";
+
+export default async function edit(req: NextApiRequest, res: NextApiResponse) {
+  // protect admin
+
+  // access body
+  const serviceIdFromClient = req.query;
+  // query edit
+
+  try {
+    //finally begin transaction
+    const { data, error } = await connectionPool.query(
+      `
+    TRANSACTION BEGIN
+    UPDATE services 
+    SET bla bla bla 
+    VALUES $1,$1,$1,$1,$1
+    UPSERT INTO sub_services (bla bla bla)
+    VALUES ($1,$1,$1,$1,$1)
+    COMMIT
+    `,
+      []
+    );
+    if (error) {
+      console.log(`Update error, see detail: ${error.message}`);
+      return res.status(400).json({ error: "Data invalid format" });
+    }
+    console.log(data);
+    return res.status(200).json({ message: "Services update successfully" });
+  } catch (error: unknown) {
+    console.log(
+      `Internal error during update data, see detail: ${error.message}`
+    );
+    return res.status(500).json({
+      error: "Internal error during update data. Fail to update data",
+    });
+  }
+}
