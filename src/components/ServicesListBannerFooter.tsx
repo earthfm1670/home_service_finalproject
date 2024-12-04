d9d9fc99661dadf22eabbd937c1fb4e5b7610606
Kittisak Saengsri <kittisaksa77@gmail.com>
2024-12-04 12:21:38 +0700

    feat: add homeservicesmalllogo.png and improve resposive design for bannerfooter

--- src/components/ServicesListBannerFooter.tsx
+++ src/components/ServicesListBannerFooter.tsx
@@ -3,18 +3,21 @@ import { Phone, Mail } from "lucide-react";
 
 const ServicesListBannerFooter: React.FC = () => {
   return (
-    <div className="flex flex-col items-center">
-      <section className="w-[375px] h-[458px] relative mt-8 px-6 py-12 overflow-hidden bg-blue-600 lg:w-[1440px] lg:h-[284px]">
-        <h1 className="absolute max-w-[327px] h-[243px] text-lg font-medium text-center text-[#FFFFFF] lg:left-[315px] lg:top-[82px] lg:flex lg:max-w-[810px] lg:max-h-[120px] ">
+    <div className="flex flex-col items-center w-full">
+      <section className="w-full h-[458px] flex justify-center relative px-6 py-12 overflow-hidden bg-blue-600 lg:h-[284px]">
+        <h1 className="absolute min-w-[327px] w-full h-[243px] sm:px-10 text-lg font-medium text-center text-[#FFFFFF] lg:left-[315px] lg:px-0 lg:top-[82px] lg:flex lg:max-w-[810px] lg:max-h-[120px] ">
           <p className="lg:hidden">
-            เพราะเราคือช่าง ผู้ให้บริการเรื่องบ้านอันดับ<br></br> 1 แบบครบวงจร
+            เพราะเราคือช่าง ผู้ให้บริการเรื่องบ้านอันดับ
+            <br className="sm:hidden"></br> 1 แบบครบวงจร
             <br></br>
-            โดยทีมช่างมืออาชีพมากกว่า 100 ทีม<br></br>
-            สามารถตอบโจทย์ด้านการบริการเรื่องบ้าน<br></br>ของคุณ และสร้าง
-            <br></br>
-            ความสะดวกสบายในการติดต่อกับทีมช่าง<br></br> ได้ทุกที่ ทุกเวลา ตลอด
-            24 ชม.<br></br>
-            มั่นใจ ช่างไม่ทิ้งงาน<br></br> พร้อมรับประกันคุณภาพงาน
+            โดยทีมช่างมืออาชีพมากกว่า 100 ทีม<br className="sm:hidden"></br>
+            สามารถตอบโจทย์ด้านการบริการเรื่องบ้าน<br className="sm:hidden"></br>
+            ของคุณ และสร้าง
+            <br className="sm:hidden"></br>
+            ความสะดวกสบายในการติดต่อกับทีมช่าง<br className="sm:hidden"></br>
+            ได้ทุกที่ ทุกเวลา ตลอด 24 ชม.<br className="sm:hidden"></br>
+            มั่นใจ ช่างไม่ทิ้งงาน<br className="sm:hidden"></br>
+            พร้อมรับประกันคุณภาพงาน
           </p>
           <p className="hidden lg:block lg:text-center lg:text-xl">
             เพราะเราคือช่าง ผู้ให้บริการเรื่องบ้านอันดับ 1 แบบครบวงจร
@@ -27,23 +30,23 @@ const ServicesListBannerFooter: React.FC = () => {
         </h1>
 
         <img
-          src="https://s3-alpha-sig.figma.com/img/f2e9/bce5/f0733b5ff94a23b59fd5fccba21d7d98?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=keODkbCXtDrRpqtrJIGHKgdtmTaacAWJVSrVrVon8qwyXxaRyt0gHvn1fC9mSfFaTBGDNOttdi~DY-SwsZy3jx~EMqLHhJz35mOr~oBbI~j1KmLtx76i3HIQ1hdXU3PGqrba1qCbONhGE4mIiU6OdNNsnwVAEDE9OREYARXiZwLpP3ITdsJh1Ak~eeKBrUfJg2TVwNFi9Jt5wO1cDigam9PdPrdMWA4Eg56cVDapXmPGrvXmUwurNsF6rSR14UUyG3anToWREJx5tq3PyxHUJFh50mlAor-qdQ5eZjEDABHPWjSuaHw6mUFoDVzeTBPux06udMCZca-wMJaMJXoXDA__"
-          alt="House"
+          src="image/homeservicelogo.png"
+          alt="homeservicelogo"
           className="absolute -bottom-16 left-1/2 transform -translate-x-1/2 w-[250px] h-[250px] opacity-20 lg:w-[416px] lg:h-[416px] lg:left-[1370px] lg:top-[-29px]"
         />
       </section>
 
-      <footer className="w-[375px] h-[389px] flex flex-col items-center justify-center lg:w-[1440px] lg:h-[193px]">
-        <div className="h-[273px] flex flex-col px-4 py-6 gap-6 lg:w-[1440px] lg:h-[151px] lg:relative">
-          <div className="flex w-full h-10 items-center gap-2 lg:absolute lg:left-40 lg:top-[46px] lg:w-64 lg:h-11">
+      <footer className="w-full h-[389px] flex flex-col items-center justify-center lg:h-[193px]">
+        <div className="min-w-[375px] w-full h-[273px] flex flex-col sm:flex-row sm:items-center sm:flex-wrap  px-4 py-6 gap-6 lg:h-[151px] lg:relative">
+          <div className="w-[253px] flex h-11 items-center gap-2 lg:absolute lg:left-40 lg:top-[46px] lg:w-64 lg:h-11 ">
             <img
-              src="https://s3-alpha-sig.figma.com/img/f2e9/bce5/f0733b5ff94a23b59fd5fccba21d7d98?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=keODkbCXtDrRpqtrJIGHKgdtmTaacAWJVSrVrVon8qwyXxaRyt0gHvn1fC9mSfFaTBGDNOttdi~DY-SwsZy3jx~EMqLHhJz35mOr~oBbI~j1KmLtx76i3HIQ1hdXU3PGqrba1qCbONhGE4mIiU6OdNNsnwVAEDE9OREYARXiZwLpP3ITdsJh1Ak~eeKBrUfJg2TVwNFi9Jt5wO1cDigam9PdPrdMWA4Eg56cVDapXmPGrvXmUwurNsF6rSR14UUyG3anToWREJx5tq3PyxHUJFh50mlAor-qdQ5eZjEDABHPWjSuaHw6mUFoDVzeTBPux06udMCZca-wMJaMJXoXDA__"
-              alt="House"
+              src="image/homeservicesmalllogo.png"
+              alt="homeservicesmalllogo"
               className="size-10 opacity-90"
             />
             <h1 className="text-3xl font-medium text-blue-600">HomeServices</h1>
           </div>
-          <div className="flex flex-col justify-between h-[77px]">
+          <div className="flex flex-col gap-2 w-[343px] sm:w-64 h-[77px]1">
             <h1 className="text-lg font-medium text-gray-950 lg:absolute lg:left-[495px] lg:top-[49px] lg:w-56 lg:h-7">
               บริษัท โฮมเซอร์วิสเซส จำกัด
             </h1>
@@ -51,23 +54,23 @@ const ServicesListBannerFooter: React.FC = () => {
               452 ซอยสุขุมวิท 79 แขวงพระโขนงเหนือ เขตวัฒนา กรุงเทพมหานคร 10260
             </p>
           </div>
-          <div className="flex flex-col justify-between h-14 lg:absolute lg:left-[1047px] lg:top-[47px]">
+          <div className="flex flex-col gap-2 w-[343px] h-14 lg:absolute lg:left-[1047px] lg:top-[47px]">
             <span>
               <p className="text-base font-normal text-gray-800 flex gap-3">
                 <Phone size={20} />
-                080-540-6357
+                080-540-63575
               </p>
             </span>
             <span>
               <p className="text-base font-normal text-gray-800 flex gap-3">
                 <Mail size={20} />
-                contact@homeservices.co
+                contact@homeservices.com
               </p>
             </span>
           </div>
         </div>
-        <div className="h-[116px] flex flex-col p-4 gap-4 lg:w-[1440px] lg:h-[42px] lg:relative bg-gray-100">
-          <span className=" w-[343px] h-[50px] flex flex-col justify-between text-sm font-normal text-gray-700 lg:absolute lg:left-[895px] lg:top-[13px] lg:flex-row lg:w-96 lg:h-[21px]">
+        <div className="w-full h-[116px] flex flex-col sm:flex-row sm:items-center sm:flex-wrap p-4 gap-4 lg:h-[42px] lg-w-[1440px] lg:relative bg-gray-100">
+          <span className=" w-[343px] h-[50px] flex flex-col justify-between text-sm font-normal text-gray-700 lg:absolute lg:right-40 lg:top-[13px] lg:flex-row lg:w-96 lg:h-[21px]">
             <p>เงื่อนไขและข้อตกลงการใช้งานเว็บไซต์</p>
             <p>นโยบายความเป็นส่วนตัว</p>
           </span>
