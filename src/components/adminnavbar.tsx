1a8d3d92cf86db7272a46d1d61f0d63b7d8dd631
PotterWinter <natthanataa@gmail.com>
2024-12-05 00:29:47 +0700

    feat: stock code version101

--- src/components/adminnavbar.tsx
+++ src/components/adminnavbar.tsx
@@ -1,68 +1,65 @@
+// export default function AdminNavbar() {
+//     return(
+//         <>
+//                     {/* navbar for admin page */}
+//                     <div className="flex flex-row items-center justify-between bg-white sticky top-0 h-20 px-10 py-5 min-w-[1200px] border-b  border-gray-300">
+//               <div className="text-xl">บริการ</div>
+//               <div className="h-full flex flex-row items-center gap-6 relative">
+//                 <div className="absolute left-3 top-1/2 transform -translate-y-1/2 ">
+//                   <IconFinding />
+//                 </div>
+//                 <input
+//                   type="text"
+//                   placeholder="ค้นหาบริการ..."
+//                   className="border border-gray-300 h-full rounded-lg w-80 pl-10"
+//                 />
+//                 <button className=" bg-defaultColor text-white text-base h-full px-7 flex items-center gap-3 rounded-lg">
+//                   เพิ่มบริการ
+//                   <span>
+//                     <IconPlus />
+//                   </span>
+//                 </button>
+//               </div>
+//             </div>
+//         </>
+//     )
+// }
 
-
-
-export default function AdminNavbar() {
-    return(
-        <>
-                    {/* navbar for admin page */}
-                    <div className="flex flex-row items-center justify-between bg-white sticky top-0 h-20 px-10 py-5 min-w-[1200px] border-b  border-gray-300">
-              <div className="text-xl">บริการ</div>
-              <div className="h-full flex flex-row items-center gap-6 relative">
-                <div className="absolute left-3 top-1/2 transform -translate-y-1/2 ">
-                  <IconFinding />
-                </div>
-                <input
-                  type="text"
-                  placeholder="ค้นหาบริการ..."
-                  className="border border-gray-300 h-full rounded-lg w-80 pl-10"
-                />
-                <button className=" bg-defaultColor text-white text-base h-full px-7 flex items-center gap-3 rounded-lg">
-                  เพิ่มบริการ
-                  <span>
-                    <IconPlus />
-                  </span>
-                </button>
-              </div>
-            </div>
-        </>
-    )
-}
-
-function IconPlus() {
-    return (
-      <svg
-        width="10"
-        height="10"
-        viewBox="0 0 10 10"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M5 0C5.26522 0 5.51957 0.105357 5.70711 0.292893C5.89464 0.48043 6 0.734784 6 1V4H9C9.26522 4 9.51957 4.10536 9.70711 4.29289C9.89464 4.48043 10 4.73478 10 5C10 5.26522 9.89464 5.51957 9.70711 5.70711C9.51957 5.89464 9.26522 6 9 6H6V9C6 9.26522 5.89464 9.51957 5.70711 9.70711C5.51957 9.89464 5.26522 10 5 10C4.73478 10 4.48043 9.89464 4.29289 9.70711C4.10536 9.51957 4 9.26522 4 9V6H1C0.734784 6 0.48043 5.89464 0.292893 5.70711C0.105357 5.51957 0 5.26522 0 5C0 4.73478 0.105357 4.48043 0.292893 4.29289C0.48043 4.10536 0.734784 4 1 4H4V1C4 0.734784 4.10536 0.48043 4.29289 0.292893C4.48043 0.105357 4.73478 0 5 0Z"
-          fill="white"
-        />
-      </svg>
-    );
-  }
+// function IconPlus() {
+//     return (
+//       <svg
+//         width="10"
+//         height="10"
+//         viewBox="0 0 10 10"
+//         fill="none"
+//         xmlns="http://www.w3.org/2000/svg"
+//       >
+//         <path
+//           fill-rule="evenodd"
+//           clip-rule="evenodd"
+//           d="M5 0C5.26522 0 5.51957 0.105357 5.70711 0.292893C5.89464 0.48043 6 0.734784 6 1V4H9C9.26522 4 9.51957 4.10536 9.70711 4.29289C9.89464 4.48043 10 4.73478 10 5C10 5.26522 9.89464 5.51957 9.70711 5.70711C9.51957 5.89464 9.26522 6 9 6H6V9C6 9.26522 5.89464 9.51957 5.70711 9.70711C5.51957 9.89464 5.26522 10 5 10C4.73478 10 4.48043 9.89464 4.29289 9.70711C4.10536 9.51957 4 9.26522 4 9V6H1C0.734784 6 0.48043 5.89464 0.292893 5.70711C0.105357 5.51957 0 5.26522 0 5C0 4.73478 0.105357 4.48043 0.292893 4.29289C0.48043 4.10536 0.734784 4 1 4H4V1C4 0.734784 4.10536 0.48043 4.29289 0.292893C4.48043 0.105357 4.73478 0 5 0Z"
+//           fill="white"
+//         />
+//       </svg>
+//     );
+//   }
   
-  function IconFinding() {
-    return (
-      <svg
-        width="20"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M19 19L13 13L19 19ZM15 8C15 8.91925 14.8189 9.82951 14.4672 10.6788C14.1154 11.5281 13.5998 12.2997 12.9497 12.9497C12.2997 13.5998 11.5281 14.1154 10.6788 14.4672C9.82951 14.8189 8.91925 15 8 15C7.08075 15 6.1705 14.8189 5.32122 14.4672C4.47194 14.1154 3.70026 13.5998 3.05025 12.9497C2.40024 12.2997 1.88463 11.5281 1.53284 10.6788C1.18106 9.82951 1 8.91925 1 8C1 6.14348 1.7375 4.36301 3.05025 3.05025C4.36301 1.7375 6.14348 1 8 1C9.85652 1 11.637 1.7375 12.9497 3.05025C14.2625 4.36301 15 6.14348 15 8Z"
-          stroke="#CCD0D7"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
-    );
-  }
+//   function IconFinding() {
+//     return (
+//       <svg
+//         width="20"
+//         height="20"
+//         viewBox="0 0 20 20"
+//         fill="none"
+//         xmlns="http://www.w3.org/2000/svg"
+//       >
+//         <path
+//           d="M19 19L13 13L19 19ZM15 8C15 8.91925 14.8189 9.82951 14.4672 10.6788C14.1154 11.5281 13.5998 12.2997 12.9497 12.9497C12.2997 13.5998 11.5281 14.1154 10.6788 14.4672C9.82951 14.8189 8.91925 15 8 15C7.08075 15 6.1705 14.8189 5.32122 14.4672C4.47194 14.1154 3.70026 13.5998 3.05025 12.9497C2.40024 12.2997 1.88463 11.5281 1.53284 10.6788C1.18106 9.82951 1 8.91925 1 8C1 6.14348 1.7375 4.36301 3.05025 3.05025C4.36301 1.7375 6.14348 1 8 1C9.85652 1 11.637 1.7375 12.9497 3.05025C14.2625 4.36301 15 6.14348 15 8Z"
+//           stroke="#CCD0D7"
+//           stroke-width="2"
+//           stroke-linecap="round"
+//           stroke-linejoin="round"
+//         />
+//       </svg>
+//     );
+//   }
