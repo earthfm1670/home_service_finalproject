fa0ac487319c27275ec1fb12fbd5b92a3586f7e3
Thanapat1502 <psy150201027@gmail.com>
2024-12-06 13:21:10 +0700

    refactor: update user/admin register api by adding metadata

--- src/utils/supabase.tsx
+++ src/utils/supabase.tsx
@@ -14,4 +14,16 @@ if (!admininKey) {
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
-export const adminSupabase = createClient(supabaseUrl, admininKey);
+export const adminSupabase = createClient(supabaseUrl, admininKey, {
+  auth: {
+    persistSession: false,
+    autoRefreshToken: false,
+    detectSessionInUrl: false,
+  },
+});
+
+//TODO Upadate all admin api using adminSupabase
+//TODO admin register: add meta data
+//TODO admin login: change into adminSupabase client
+//TODO user register: add meta data
+//TODO check if nessessary to updatre CRUD for admin api
