0c24f253a104d8056b4bf578288c0c5b99b256b2
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-12-06 13:50:00 +0700

    add: replace homepage service list with rendering list from servicelist

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,18 @@ import HomeHero from "@/components/homehero";
 import HomeJoinUs from "@/components/homejoinus";
 import HomeService from "@/components/homeservice";
 import { Navbar } from "@/components/navbar";
+import { ServicesProvider } from "@/components/ServicesContext";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <HomeHero />
-      <HomeService />
-      <HomeJoinUs />
-      <HomeFooter />
+      <ServicesProvider>
+        <Navbar />
+        <HomeHero />
+        <HomeService />
+        <HomeJoinUs />
+        <HomeFooter />
+      </ServicesProvider>
     </>
   );
 }
