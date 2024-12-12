e8f379cb33db252b034bc41df025d2063eaa03ab
Kittisak Saengsri <kittisaksa77@gmail.com>
2024-12-12 22:26:56 +0700

    feat: add sort by popular services and recommended services

--- src/components/ServicesContext.tsx
+++ src/components/ServicesContext.tsx
@@ -36,17 +36,17 @@ export const ServicesProvider: React.FC<{ children: ReactNode }> = ({
 
   const getServicesData = async (
     searchValue = "",
-    selectCategory = "",
-    sortBy = "",
+    selectCategory = "บริการทั้งหมด",
+    sortBy = "popular",
     priceRange: [Number, Number] = [0, 2000]
   ) => {
     try {
       console.log(
-        `Seleted Category = ${selectCategory}, Sort By = ${sortBy}, Price Range Min = ${priceRange[0]} Max= ${priceRange[1]} SearchText = ${searchValue}`
+        `Context : Category = ${selectCategory}, Sort By = ${sortBy}, Price Range Min = ${priceRange[0]} Max= ${priceRange[1]} SearchText = ${searchValue}`
       ); // log ดู parameter ที่ส่งเข้ามาผ่าน context เพื่อใช้ query data
 
       const response = await axios.get(
-        `api/services?limit=9&search=${searchValue}&category=${selectCategory}`
+        `api/services?limit=9&search=${searchValue}&category=${selectCategory}&sort_by=${sortBy}`
       );
       setServicesData(response.data.data);
       console.log("API response:", response.data.data); // log api response จาก database
