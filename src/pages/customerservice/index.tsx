47bf4bfe47fea76870ff3a46353d82b5a2b7a541
Thanapat1502 <psy150201027@gmail.com>
2024-12-12 15:22:22 +0700

    fix: adjust sidbar position and make it activable

--- src/pages/customerservice/index.tsx
+++ src/pages/customerservice/index.tsx
@@ -5,12 +5,15 @@ export default function customerProfile() {
   return (
     <>
       <Navbar />
-      <div className="banner flex flex-row justify-center items-center w-full bg-blue-600 font-medium text-3xl text-white py-6">
+      <div className="banner hidden lg:flex justify-center items-center w-full bg-blue-600 font-medium text-3xl text-white py-6">
         ข้อมูลผู้ใช้งาน
       </div>
       <div className="template-body bg-[#F3F4F6] lg:flex lg:relative lg:pb-24 lg:pl-44 lg:pr-40">
-        <div className="sidebar-box flex justify-center w-fit lg:fixed lg:top-44">
+        <div className="sidebar-box flex justify-center lg:fixed lg:top-44">
           <UserSidebar />
+        </div>
+        <div className="small-banner flex lg:hidden justify-center items-center border rounded-lg mx-4 my-4 bg-blue-600 font-medium text-3xl text-white py-6">
+          ข้อมูลผู้ใช้งาน
         </div>
         <div
           className="content bg-green-400 h-[5000px] 
