ae4c0cc8374a7e1506b2a04c55504c0327f29570
Thanapat1502 <psy150201027@gmail.com>
2024-12-12 14:16:50 +0700

    feature: create customer service pages template for porfile, orderlist, history

---
+++ src/pages/customerservice/orderlist/index.tsx
@@ -0,0 +1,22 @@
+import { Navbar } from "@/components/navbar";
+import HomeFooter from "@/components/homefooter";
+import UserSidebar from "@/components/customer/userSidebar";
+export default function customerOrderlist() {
+  return (
+    <>
+      <Navbar />
+      <div className="banner flex flex-row justify-center items-center w-full bg-blue-600 font-medium text-3xl text-white py-6">
+        รายการคำสั่งซ่อม
+      </div>
+      <div className="template-body bg-[#F3F4F6] lg:flex lg:relative lg:pb-24 lg:pl-44 lg:pr-40">
+        <div className="sidebar-box flex justify-center w-fit lg:fixed lg:top-44">
+          <UserSidebar />
+        </div>
+        <div
+          className="content bg-green-400 h-[5000px] 
+        flex justify-center w-full lg:ml-72"></div>
+      </div>
+      <HomeFooter />
+    </>
+  );
+}
