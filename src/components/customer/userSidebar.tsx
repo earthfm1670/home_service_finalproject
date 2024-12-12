12d0912888616611325daa217342d4601c7533e5
Thanapat1502 <psy150201027@gmail.com>
2024-12-12 15:22:22 +0700

    fix: adjust sidbar position and make it activable

--- src/components/customer/userSidebar.tsx
+++ src/components/customer/userSidebar.tsx
@@ -1,23 +1,90 @@
+import { useRouter } from "next/router";
+import {
+  UserRound as UserIcon,
+  ClipboardList as ListIcon,
+  History as HistoryIcon,
+} from "lucide-react";
 //TODO ลบสี
 //TODO เพิ่ม icon
 //TODO แก้ margin header
 export default function UserSidebar() {
+  const router = useRouter();
+  const handleProfile = (): void => {
+    router.push("/customerservice");
+  };
+  const handleOrderList = (): void => {
+    router.push("/customerservice/orderlist");
+  };
+  const handleHistory = (): void => {
+    router.push("/customerservice/history");
+  };
+
   return (
     <>
-      <div className="sidebar-body bg-white flex flex-col justify-start items-start pl-4 pr-9 w-11/12 lg:w-64 lg:h-64 border rounded-lg">
+      <div
+        className="sidebar-body bg-white flex flex-col justify-start items-start 
+      my-2 pl-4 pr-9 w-11/12
+      lg:w-64 lg:h-64 border rounded-lg">
         <div className="header-box border-b w-full mx-4">
           <h3 className="sidebar-heade font-normal text-xl text-gray-700 p-6">
             บัญชีผู้ใช้
           </h3>
         </div>
         <div className="sidebar-buttons mx-6 my-4 flex flex-row justify-between lg:flex-col">
-          <button className="bg-red-200 font-normal text-base text-gray-950 py-3 w-32 lg:w-44  text-start">
+          <button
+            className={`font-normal text-base py-3 pr-5 pl-1 w-32 lg:w-44 text-start
+            flex justify-start items-center gap-2 ${
+              router.pathname === "/customerservice"
+                ? `text-blue-700`
+                : `text-gray-950`
+            } `}
+            onClick={handleProfile}>
+            <span>
+              <UserIcon
+                size={24}
+                color={router.pathname === "/customerservice" ? "blue" : "grey"}
+              />
+            </span>
             ข้อมูลผู้ใช้งาน
           </button>
-          <button className="bg-yellow-200 font-normal text-base text-gray-950 py-3 w-32 lg:w-44  text-start">
+          <button
+            className={`font-normal text-base py-3 pr-5 pl-1 w-32 lg:w-44 text-start 
+            flex justify-start items-center gap-2 ${
+              router.pathname === "/customerservice/orderlist"
+                ? `text-blue-700`
+                : `text-gray-950`
+            }`}
+            onClick={handleOrderList}>
+            <span>
+              <ListIcon
+                size={24}
+                color={
+                  router.pathname === "/customerservice/orderlist"
+                    ? "blue"
+                    : "grey"
+                }
+              />
+            </span>
             รายการคำสั่งซ่อม
           </button>
-          <button className="bg-green-200 font-normal text-base text-gray-950 py-3 w-32 lg:w-44  text-start">
+          <button
+            className={`font-normal text-base py-3 pr-5 pl-1 w-32 lg:w-44 text-start 
+            flex justify-start items-center gap-2 ${
+              router.pathname === "/customerservice/history"
+                ? `text-blue-700`
+                : `text-gray-950`
+            } `}
+            onClick={handleHistory}>
+            <span>
+              <HistoryIcon
+                size={24}
+                color={
+                  router.pathname === "/customerservice/history"
+                    ? "blue"
+                    : "grey"
+                }
+              />
+            </span>
             ประวัติการซ่อม
           </button>
         </div>
