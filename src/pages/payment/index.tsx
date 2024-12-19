d2d9808d5ce3f2d74ed0a8aa189578d445ceabb3
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-12-19 12:23:33 +0700

    add: sticky summary for mobile

--- src/pages/payment/index.tsx
+++ src/pages/payment/index.tsx
@@ -4,6 +4,7 @@ import PaymentForm from "@/components/PaymentForm";
 import { Navbar } from "@/components/navbar";
 import { ProgressStepsNew } from "@/components/service-detail/ProgressSteps";
 import Link from "next/link";
+import MobileSummary from "@/components/service-detail/MobileSummary";
 
 //stand alone payment page, need to connect to service info later
 const PaymentPage: React.FC = () => {
@@ -11,6 +12,14 @@ const PaymentPage: React.FC = () => {
     service_picture_url: string;
     service_name: string;
   } | null>(null);
+
+  const getSelectedServices = () => [
+    { id: 1, description: "Service A" },
+    { id: 2, description: "Service B" },
+  ];
+
+  const getQuantityDisplay = (subServiceId: number) => 2;
+  const calculateTotal = () => 1500;
 
   const [currentStep, setCurrentStep] = useState(3);
 
@@ -28,38 +37,49 @@ const PaymentPage: React.FC = () => {
 
   return (
     <StripeContext>
-      {/* <h1>Payment Page</h1> */}
-      <Navbar />
-      {/* Hero Section */}
-      <div className="relative h-[168px] w-full lg:h-56">
-        <img
-          src={service.service_picture_url}
-          alt={service.service_name}
-          className="w-full h-full object-cover"
-        />
-        <div className="absolute inset-0 bg-[#163C9366]">
-          <div className="px-4 pt-9 lg:px-32 lg:mt-5">
-            {/* Breadcrumb */}
-            <div className="">
-              <div className="bg-white rounded-md py-2 px-4 inline-flex items-center space-x-2 lg:p-5">
-                <Link
-                  href="/services"
-                  className="text-gray-500 hover:text-blue-600 text-sm"
-                >
-                  บริการของเรา
-                </Link>
-                <span className="text-gray-500 text">&gt;</span>
-                <span className="text-blue-600 font-bold text-3xl">
-                  {service.service_name}
-                </span>
+      <div className="flex flex-col">
+        {/* <h1>Payment Page</h1> */}
+        <Navbar />
+        {/* Hero Section */}
+        <div className="relative h-[168px] w-full lg:h-56">
+          <img
+            src={service.service_picture_url}
+            alt={service.service_name}
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute inset-0 bg-[#163C9366]">
+            <div className="px-4 pt-9 lg:px-32 lg:mt-5">
+              {/* Breadcrumb */}
+              <div className="">
+                <div className="bg-white rounded-md py-2 px-4 inline-flex items-center space-x-2 lg:p-5">
+                  <Link
+                    href="/services"
+                    className="text-gray-500 hover:text-blue-600 text-sm"
+                  >
+                    บริการของเรา
+                  </Link>
+                  <span className="text-gray-500 text">&gt;</span>
+                  <span className="text-blue-600 font-bold text-3xl">
+                    {service.service_name}
+                  </span>
+                </div>
               </div>
-            </div>
 
-            <ProgressStepsNew currentStep={currentStep} />
+              <ProgressStepsNew currentStep={currentStep} />
+            </div>
           </div>
         </div>
       </div>
-      <PaymentForm />
+      <div className="flex-1">
+        <PaymentForm />
+      </div>
+      <div className="sticky bottom-0 z-10">
+        <MobileSummary
+          getSelectedServices={getSelectedServices}
+          getQuantityDisplay={getQuantityDisplay}
+          calculateTotal={calculateTotal}
+        />
+      </div>
     </StripeContext>
   );
 };
