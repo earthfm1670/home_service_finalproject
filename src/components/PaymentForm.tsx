8467f4db06866fd2c07f834c816f9001c7c02a05
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-12-21 18:51:40 +0700

    add: payment form and desktopSummary into a flexbox

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -108,7 +108,7 @@ const PaymentForm: React.FC<PaymentFormProps> = ({ setDiscount }) => {
   };
 
   return (
-    <div className="border mt-14 mx-4 mb-10 rounded-lg bg-white lg:w-[735px] lg:mt-28">
+    <div className="border mx-4 mb-10 rounded-lg bg-white lg:w-[735px]">
       <div className="mx-3 mt-2 text-[18px] text-gray-700 font-medium lg:mx-6 lg:text-[20px] lg:py-6 lg:hidden">
         กรอกข้อมูลบริการ
       </div>
@@ -270,11 +270,6 @@ const PaymentForm: React.FC<PaymentFormProps> = ({ setDiscount }) => {
           </div>
         </>
       )}
-
-      {/* <button type="submit" disabled={!stripe || loading}>
-          {loading ? "Processing..." : "Pay"}
-        </button> */}
-      {/* {error && <div>{error}</div>} */}
     </div>
   );
 };
--- src/pages/payment/index.tsx
+++ src/pages/payment/index.tsx
@@ -152,8 +152,21 @@ const PaymentPage: React.FC = ({ initialService }: ServiceInfoPageProps) => {
         {/* Hero Section */}
         <ServiceHero service={service} />
       </div>
-      <div className="flex-1 overflow-y-auto pb-28 bg-gray-100">
-        <PaymentForm setDiscount={setDiscount} />
+      <div className="mt-14 lg:flex lg:mt-28 lg:flex-row lg:justify-center lg:gap-20">
+        <div className=" overflow-y-auto pb-28 lg:pb-10">
+          <PaymentForm setDiscount={setDiscount} />
+        </div>
+        <div className="lg:w-[349px] lg:h-[374px]">
+          <DesktopSummary
+            getSelectedServices={getSelectedServices}
+            getQuantityDisplay={getQuantityDisplay}
+            calculateTotal={calculateTotal}
+            canProceed={isFormComplete()}
+            handleProceed={handleProceed}
+            locationInfo={locationInfo}
+            discount={discount}
+          />
+        </div>
       </div>
       <div className="sticky bottom-0 z-10">
         <div className="px-4 py-8 mt-4 lg:mt-16 lg:px-32">
