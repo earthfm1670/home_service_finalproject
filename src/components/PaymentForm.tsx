01577735d1b669fca48b8056d466691cb132f0f2
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2024-12-21 18:11:37 +0700

    fix: payment input styling, adjust stripe input element

--- src/components/PaymentForm.tsx
+++ src/components/PaymentForm.tsx
@@ -83,6 +83,8 @@ const PaymentForm: React.FC<PaymentFormProps> = ({ setDiscount }) => {
       fontWeight: "normal",
       color: "#424770",
       fontFamily: "'Poppins', sans-serif",
+      lineHeight: "28px",
+      padding: "0 12px",
       "::placeholder": {
         color: "#6B7280",
         fontSize: "16px",
@@ -96,7 +98,7 @@ const PaymentForm: React.FC<PaymentFormProps> = ({ setDiscount }) => {
   };
 
   const customCardNumberOptions = {
-    ...elementStyle,
+    style: elementStyle,
     placeholder: "XXXX XXXX XXXX XXXX",
   };
 
@@ -106,14 +108,17 @@ const PaymentForm: React.FC<PaymentFormProps> = ({ setDiscount }) => {
   };
 
   return (
-    <div className="border mt-14 mx-4 rounded-lg bg-white">
-      <div className="mx-3 mt-2 text-[18px] text-gray-700 font-medium">
+    <div className="border mt-14 mx-4 rounded-lg bg-white lg:w-[735px] lg:mt-28">
+      <div className="mx-3 mt-2 text-[18px] text-gray-700 font-medium lg:mx-6 lg:text-[20px] lg:py-6 lg:hidden">
         กรอกข้อมูลบริการ
       </div>
-      <div className="mx-3 mb-2 h-[95px] flex justify-between">
+      <div className="mx-3 mt-2 text-[18px] text-gray-700 font-medium lg:mx-6 lg:text-[20px] lg:py-6 hidden lg:block">
+        ชำระเงิน
+      </div>
+      <div className="mx-3 mb-2 h-[95px] flex justify-between lg:justify-center lg:gap-6">
         {/* Promptpay Option */}
         <div
-          className={`w-[147px] h-[95px] border rounded-sm flex flex-col justify-center items-center cursor-pointer ${
+          className={`w-[147px] h-[95px] lg:w-[331px] lg:h-[86px] border rounded-sm flex flex-col justify-center items-center cursor-pointer ${
             selectedPayment === "qrcode" ? "bg-blue-100 border-blue-600" : ""
           }`}
           onClick={() => setSelectedPayment("qrcode")}
@@ -139,7 +144,7 @@ const PaymentForm: React.FC<PaymentFormProps> = ({ setDiscount }) => {
         </div>
         {/* Credit Card Option */}
         <div
-          className={`w-[147px] h-[95px] border rounded-sm flex flex-col justify-center items-center cursor-pointer ${
+          className={`w-[147px] h-[95px] lg:w-[331px] lg:h-[86px] border rounded-sm flex flex-col justify-center items-center cursor-pointer ${
             selectedPayment === "creditcard"
               ? "bg-blue-100 border-blue-600"
               : ""
@@ -173,38 +178,42 @@ const PaymentForm: React.FC<PaymentFormProps> = ({ setDiscount }) => {
       {selectedPayment === "creditcard" ? (
         <>
           <form onSubmit={handleSubmit} className="bg-white">
-            <div className="mx-3 my-2">
+            <div className="mx-3 my-2 lg:mx-6">
               <label className="block">
                 หมายเลขบัตรเครดิต<span className="text-[#C82438]">*</span>
               </label>
               <CardNumberElement
                 options={customCardNumberOptions}
-                className="block w-full border border-gray-300 rounded-md py-2 px-2"
+                className="block w-full h-[44px] border border-gray-300 rounded-md py-2 px-2"
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
               />
             </div>
-            <div className="mx-3">
+            <div className="mx-3 lg:mx-6">
               <label className="block mb-2">
                 ชื่อบนบัตร<span className="text-[#C82438]">*</span>
                 <input
                   type="text"
                   value={name}
                   onChange={(e) => setName(e.target.value)}
-                  className="block w-full border border-gray-300 rounded-md py-1 placeholder:text-gray-500 placeholder:text-[16px] placeholder:font-normal placeholder:px-2"
+                  className="block w-full h-[44px] border border-gray-300 rounded-md py-1 placeholder:text-gray-500 placeholder:text-[16px] placeholder:font-normal placeholder:px-2"
                   placeholder="กรุณากรอกชื่อบนบัตร"
                 ></input>
               </label>
             </div>
             <div>
-              <div className="mx-3 mb-4">
+              <div className="mx-3 mb-4 lg:mx-6">
                 <label className="block">
                   วันหมดอายุ<span className="text-[#C82438]">*</span>
                 </label>
                 <CardExpiryElement
                   options={{ style: elementStyle }}
-                  className="block w-full border border-gray-300 rounded-md py-2 px-2"
+                  className="block w-full h-[44px] border border-gray-300 rounded-md py-2 px-2"
                 />
               </div>
-              <div className="mx-3 mb-4">
+              <div className="mx-3 mb-4 lg:mx-6">
                 <label className="block">
                   รหัส CVC / CVV<span className="text-[#C82438]">*</span>
                 </label>
@@ -214,15 +223,15 @@ const PaymentForm: React.FC<PaymentFormProps> = ({ setDiscount }) => {
                 />
               </div>
               {/* divider line */}
-              <div className="border-t border-gray-300 my-6 mb-8 mx-3"></div>
-              <div className="mx-3 mb-6">
+              <div className="border-t border-gray-300 my-6 mb-8 mx-3 lg:mx-6"></div>
+              <div className="mx-3 mb-6 lg:mx-6">
                 <label>Promotion Code</label>{" "}
                 <div className="flex items-center gap-4">
                   <input
                     type="text"
                     value={promoCode}
                     onChange={(e) => setPromoCode(e.target.value)}
-                    className="block min-w-[205px] h-[64px] text-[16px] border border-gray-300 rounded-md py-3 px-2 placeholder:text-[16px]"
+                    className="block min-w-[205px] h-[64px] text-[16px] border border-gray-300 rounded-md py-3 px-2 placeholder:text-[16px] lg:h-[44px]"
                     placeholder="กรุณากรอกโค้ดส่วนลด(ถ้ามี)"
                   ></input>
                   <button
@@ -246,7 +255,7 @@ const PaymentForm: React.FC<PaymentFormProps> = ({ setDiscount }) => {
                 type="text"
                 value={promoCode}
                 onChange={(e) => setPromoCode(e.target.value)}
-                className="block min-w-[205px] h-[64px] text-[16px] border border-gray-300 rounded-md py-3 px-2 placeholder:text-[16px]"
+                className="block min-w-[205px] h-[64px] lg:h-[44px] text-[16px] border border-gray-300 rounded-md py-3 px-2 placeholder:text-[16px]"
                 placeholder="กรุณากรอกโค้ดส่วนลด(ถ้ามี)"
               ></input>
               <button
