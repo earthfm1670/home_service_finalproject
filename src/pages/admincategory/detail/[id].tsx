73770963d6589ef177561db272e3f28cab206a36
PotterWinter <natthanataa@gmail.com>
2024-12-23 12:54:46 +0700

    fix: button for admin category detail

--- src/pages/admincategory/detail/[id].tsx
+++ src/pages/admincategory/detail/[id].tsx
@@ -224,17 +224,17 @@ export default function AdminNavbar() {
             <div className="flex flex-row items-center justify-between bg-white sticky top-0 h-20 px-10 py-5 min-w-[1200px] border-b  border-gray-300 z-10">
               <div className="text-xl">เพิ่มหมวดหมู่</div>
               <div className="h-full flex flex-row items-center gap-6 relative">
-                <button
+                {/* <button
                   className=" bg-white text-defaultColor text-base h-full px-7 flex items-center gap-3 rounded-lg w-32 text-center justify-center border border-defaultColor"
                   onClick={() => router.push("/admincategory")}
                 >
                   ยกเลิก
-                </button>
+                </button> */}
                 <button
                   className=" bg-defaultColor text-white text-base h-full px-7 flex items-center gap-3 rounded-lg w-32 text-center justify-center "
-                  type="submit"
+                  onClick={() => router.push(`/admincategory/edit/${id}`)}
                 >
-                  สร้าง
+                  แก้ไข
                 </button>
               </div>
             </div>
