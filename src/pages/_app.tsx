061411c39db05a2d86cebdb789705d639301b1d4
Thanapat1502 <psy150201027@gmail.com>
2024-12-25 14:41:58 +0700

    fix: adjust axios interceptor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
+import jwtInterceptor from "@/utils/jwtInterseptor";
 import { AuthProvider } from "@/context/authContext";
 
 export default function App({ Component, pageProps }: AppProps) {
+  jwtInterceptor();
   return (
     <AuthProvider>
       <Component {...pageProps} />
