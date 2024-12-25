c5e98f4e8af2dfa00eb8a90b404cab54534aa366
Thanapat1502 <psy150201027@gmail.com>
2024-12-25 14:41:58 +0700

    fix: adjust axios interceptor

--- src/utils/jwtInterseptor.ts
+++ src/utils/jwtInterseptor.ts
@@ -1,54 +1,46 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
+
 //Solution
 /**
  * 1. Create new axios interceptor
  * - Pros: Solid solution for current practics (jwt token, verify token per req, etc.)
  * - Cons: Have to change all axios across entire wep app.
- * 
+ *
  * 2. Verify token via cookies
  * - Pros: Fast and seem to be potential solution for current practics
  * - Cons: Have to research agin, might have to change auth system (token storing? only?)
- * 
+ *
  * 3. High Order Function
  * - Pros: Have solid example, can discus with other group
  * - Cons: New practics, might have to change auth system (unknow)
+ *
+ * 4. create funtion for interceptor
+ * - Pros: check if function work
  */
 
 //Add a request interceptor
-axios.interceptors.request.use(
-  function (config: InternalAxiosRequestConfig) {
+function jwtInterceptor() {
+  axios.interceptors.request.use((req: InternalAxiosRequestConfig) => {
     const token = window.localStorage.getItem("token");
     if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
-      //config.headers.set("Authorization", `Bearer ${token}`);
+      req.headers?.set(`Authorization`, `Bearer ${token}`);
     }
-    return config;
-  },
+    return req;
+  });
 
-  function (error) {
-    // Do something with request error
-    error = { ...error, MESSAGE: "error from interceptor" };
-    return Promise.reject(error);
-  }
-);
-
-// // Add a response interceptor | Can use to logout
-axios.interceptors.response.use(
-  function (response) {
-    console.log("Interceptor Work");
-    console.log("************************************************");
-    return response;
-  },
-  function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    console.log("Interceptor Error");
-    console.log("************************************************");
-    return Promise.reject(error);
-  }
-);
-
-export default axios;
+  // // Add a response interceptor | Can use to logout
+  axios.interceptors.response.use(
+    (res) => {
+      console.log("Interceptor Work");
+      console.log("************************************************");
+      return res;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+}
+export default jwtInterceptor;
 //*---TechUp-------------------------------------- */
 // axios.interceptors.request.use((req) => {
 //   const token = Boolean(window.localStorage.getItem("token"));
