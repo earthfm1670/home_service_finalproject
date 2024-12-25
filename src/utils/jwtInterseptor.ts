8a3fc42a4be931788dd22c44ea7fb3a55c2b97ba
Thanapat1502 <psy150201027@gmail.com>
2024-12-25 14:41:58 +0700

    fix: adjust axios interceptor

--- src/utils/jwtInterseptor.ts
+++ src/utils/jwtInterseptor.ts
@@ -1,17 +1,75 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
+
 //Solution
 /**
  * 1. Create new axios interceptor
  * - Pros: Solid solution for current practics (jwt token, verify token per req, etc.)
  * - Cons: Have to change all axios across entire wep app.
- * 
+ *
  * 2. Verify token via cookies
  * - Pros: Fast and seem to be potential solution for current practics
  * - Cons: Have to research agin, might have to change auth system (token storing? only?)
- * 
+ *
  * 3. High Order Function
  * - Pros: Have solid example, can discus with other group
  * - Cons: New practics, might have to change auth system (unknow)
+ *
+ * 4. create funtion for interceptor
+ * - Pros: check if function work
+ */
+
+//Solution
+/**
+ * 1. Create new axios interceptor
+ * - Pros: Solid solution for current practics (jwt token, verify token per req, etc.)
+ * - Cons: Have to change all axios across entire wep app.
+ *
+ * 2. Verify token via cookies
+ * - Pros: Fast and seem to be potential solution for current practics
+ * - Cons: Have to research agin, might have to change auth system (token storing? only?)
+ *
+ * 3. High Order Function
+ * - Pros: Have solid example, can discus with other group
+ * - Cons: New practics, might have to change auth system (unknow)
+ *
+ * 4. create funtion for interceptor
+ * - Pros: check if function work
+ */
+
+//Solution
+/**
+ * 1. Create new axios interceptor
+ * - Pros: Solid solution for current practics (jwt token, verify token per req, etc.)
+ * - Cons: Have to change all axios across entire wep app.
+ *
+ * 2. Verify token via cookies
+ * - Pros: Fast and seem to be potential solution for current practics
+ * - Cons: Have to research agin, might have to change auth system (token storing? only?)
+ *
+ * 3. High Order Function
+ * - Pros: Have solid example, can discus with other group
+ * - Cons: New practics, might have to change auth system (unknow)
+ *
+ * 4. create funtion for interceptor
+ * - Pros: check if function work
+ */
+
+//Solution
+/**
+ * 1. Create new axios interceptor
+ * - Pros: Solid solution for current practics (jwt token, verify token per req, etc.)
+ * - Cons: Have to change all axios across entire wep app.
+ *
+ * 2. Verify token via cookies
+ * - Pros: Fast and seem to be potential solution for current practics
+ * - Cons: Have to research agin, might have to change auth system (token storing? only?)
+ *
+ * 3. High Order Function
+ * - Pros: Have solid example, can discus with other group
+ * - Cons: New practics, might have to change auth system (unknow)
+ *
+ * 4. create funtion for interceptor
+ * - Pros: check if function work
  */
 
 //Solution
