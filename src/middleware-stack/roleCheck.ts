533793c8f3a85daa238e0bc5a7eb275c40818e2f
Thanapat1502 <psy150201027@gmail.com>
2024-12-26 14:26:37 +0700

    feature: align mw in order

--- src/middleware-stack/roleCheck.ts
+++ src/middleware-stack/roleCheck.ts
@@ -1,13 +1,16 @@
-import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export default function roleCheck(req: NextRequest) {
   console.log("From rolecheck");
   const user = req.headers.get("userPayload");
-  const payload = JSON.parse(user);
-  console.log(`Payload from role check.`);
-  console.log(payload);
-  console.log(`________________________`);
+  if (user) {
+    const payload = JSON.parse(user);
+    console.log(`Payload from role check.`);
+    console.log(payload);
+    console.log(`________________________`);
+  } else {
+    console.log("Role: Unauthenticated");
+  }
 
   return NextResponse.next();
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,30 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
+import chain from "./middleware-stack/chain";
 import authorizationCheck from "./middleware-stack/authorizationCheck";
 import roleCheck from "./middleware-stack/roleCheck";
+import dummy1 from "./middleware-stack/dummyMW/dummy1";
+import dummy2 from "./middleware-stack/dummyMW/dummy2";
 //create axios interceptor for frontend
-export function middleware(req: NextRequest) {
-  //1. modify req, add header
-  //handle by axios intercept
-  //2. authorizationCheck(req);
+
+// export function middleware() {
+//   //1. modify req, add header
+//   //handle by axios intercept
+//   //2. authorizationCheck(req);
+//   //3. Check Role
+//   //4. return
+//   //================================================
+//   const middlewareFactory = [authorizationCheck, roleCheck];
+//   chain(middlewareFactory);
+
+//   return NextResponse.next();
+// }
+
+export default function middleware(req: NextRequest) {
+  console.log("From MW***********************************");
   authorizationCheck(req);
-  //3. Check Role
   roleCheck(req);
-  //4. return
+  // dummy1(req);
+  // dummy2(req);
   return NextResponse.next();
 }
