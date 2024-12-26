cdc014be1c1d2fe66ccd74d7d0a08dabc25b6c88
Thanapat1502 <psy150201027@gmail.com>
2024-12-26 11:25:00 +0700

    feat: create role check middleware

--- src/middleware-stack/authorizationCheck.ts
+++ src/middleware-stack/authorizationCheck.ts
@@ -1,16 +1,33 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+import roleCheck from "./roleCheck";
 export default function authorizationCheck(req: NextRequest) {
-  if (req.headers) {
-    // const url = req.url;
-    // const pathName = req.nextUrl;
-    console.log("-V--V--V-Header--V--V--V----------------");
-    console.log(req);
-
-    console.log(req.cookies);
-    console.log("-A--A--A------------------------");
-  } else {
-    console.log("No header");
+  //1. access authorization
+  const authorization = req.headers.get("authorization");
+  if (!authorization || authorization.startsWith(`Bearer `)) {
+    return NextResponse.next();
   }
-  return NextResponse.next();
+  // 2. check if token is valid
+  const trimedToken = authorization.split(" ")[1];
+  console.log(`From auth check.`);
+  //2.1 check if sercret key is exit for the sake of TS
+  const secretKey = process.env.SECRET_KEY;
+  if (!secretKey) {
+    console.error("SECRET_KEY is not defined in environment variables");
+    return NextResponse.json({
+      status: 500,
+      message: "Internal Server Error",
+    });
+  }
+
+  jwt.verify(trimedToken, secretKey, (err, payload) => {
+    if (err) {
+      return NextResponse.json({ status: 403, message: err.message });
+    }
+    req.headers.set(`userPayload`, JSON.stringify(payload));
+  });
+
+  // 3. set payload and userRole to req
+  return NextResponse.next(roleCheck(req));
 }
