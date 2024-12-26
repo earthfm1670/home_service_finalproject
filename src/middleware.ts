c7269147f46f7ba88aa5479f2e1ead8a2228e5ee
Thanapat1502 <psy150201027@gmail.com>
2024-12-26 11:25:00 +0700

    feat: create role check middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import authorizationCheck from "./middleware-stack/authorizationCheck";
+import roleCheck from "./middleware-stack/roleCheck";
 //create axios interceptor for frontend
 export function middleware(req: NextRequest) {
   //1. modify req, add header
@@ -8,5 +9,7 @@ export function middleware(req: NextRequest) {
   //2. authorizationCheck(req);
   authorizationCheck(req);
   //3. Check Role
+  roleCheck(req);
+  //4. return
   return NextResponse.next();
 }
