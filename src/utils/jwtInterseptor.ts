1c5c5f3bed8563ca09febc41d8ca22be20c76840
Thanapat1502 <psy150201027@gmail.com>
2024-12-27 17:28:35 +0700

    feature: add path protection mw

--- src/utils/jwtInterseptor.ts
+++ src/utils/jwtInterseptor.ts
@@ -20,13 +20,20 @@ import axios, { InternalAxiosRequestConfig } from "axios";
 
 //Add a request interceptor
 function jwtInterceptor() {
-  axios.interceptors.request.use((req: InternalAxiosRequestConfig) => {
-    const token = window.localStorage.getItem("token");
-    if (token) {
-      req.headers?.set(`Authorization`, `Bearer ${token}`);
+  axios.interceptors.request.use(
+    (req: InternalAxiosRequestConfig) => {
+      console.log("FROM AXIOS INTERCEPTOR");
+      const token = window.localStorage.getItem("token");
+      console.log("Form Interceptor: ", token);
+      if (token) {
+        req.headers?.set(`Authorization`, `Bearer ${token}`);
+      }
+      return req;
+    },
+    (error) => {
+      return Promise.reject(error);
     }
-    return req;
-  });
+  );
 
   // // Add a response interceptor | Can use to logout
   axios.interceptors.response.use(
