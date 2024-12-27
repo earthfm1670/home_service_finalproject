543d525d88c37c9165d0943b693cab6807cc5320
Thanapat1502 <psy150201027@gmail.com>
2024-12-27 17:28:35 +0700

    feature: add path protection mw

--- src/middleware-stack/authorizationCheck.ts
+++ src/middleware-stack/authorizationCheck.ts
@@ -1,18 +1,18 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
-import jwt from "jsonwebtoken";
+import { jwtVerify } from "jose";
 
-export default function authorizationCheck(req: NextRequest) {
+export default async function authorizationCheck(req: NextRequest) {
   //1. access authorization
-  const authorization = req.headers.get("authorization");
-  if (!authorization || authorization.startsWith(`Bearer `)) {
+  const authorization = req.headers.get("Authorization");
+  if (!authorization || !authorization.startsWith(`Bearer `)) {
     console.log("user has NO token--------------------------------");
     return NextResponse.next();
   }
 
   // 2. check if token is valid
   const trimedToken = authorization.split(" ")[1];
-  console.log(`From auth check.`);
+  console.log(`Auth check. I`);
   //2.1 check if sercret key is exit for the sake of TS
   const secretKey = process.env.SECRET_KEY;
   if (!secretKey) {
@@ -22,14 +22,49 @@ export default function authorizationCheck(req: NextRequest) {
       message: "Internal Server Error",
     });
   }
-
+  console.log(`Auth check. II`);
   // 3. set payload and userRole to req
-  jwt.verify(trimedToken, secretKey, (err, payload) => {
-    if (err) {
-      return NextResponse.json({ status: 403, message: err.message });
-    }
-    req.headers.set(`userPayload`, JSON.stringify(payload));
-  });
+  // const payload = await new Promise<any>((resolve, reject) => {
+  //   jwt.verify(trimedToken, secretKey, (err, decode) => {
+  //     console.log(`Auth check. III`);
+  //     if (err) {
+  //       console.log(`Auth check.IV`);
+  //       reject(`Auth check. III ${err.message}`);
+  //     } else {
+  //       resolve(decode);
+  //     }
+  //   });
+  // });
+  const { payload } = await jwtVerify(
+    trimedToken,
+    new TextEncoder().encode(secretKey)
+  );
+  req.headers.set(`userRole`, JSON.stringify(payload));
+  //response.headers.set(`userPayload`, JSON.stringify(payload)); << not work
+  console.log(`Auth check. III`);
   console.log("user has token---------------------------------------");
   return NextResponse.next();
 }
+//PASS
+
+/** AFTER FINISHING: ADD THIS CODE BELOW TRY CATCH TO DETECTH EXPIRED TOKEN
+ * try {
+    const { payload } = await jwtVerify(
+      trimedToken,
+      new TextEncoder().encode(secretKey)
+    );
+    const response = NextResponse.next();
+    response.headers.set(`userPayload`, JSON.stringify(payload));
+    console.log("user has token---------------------------------------");
+    return response;
+  } catch (err) {
+    if (err.code === "ERR_JWT_EXPIRED") {
+      console.log("JWT has expired");
+      return NextResponse.json({
+        status: 401,
+        message: "Token has expired. Please log in again.",
+      });
+    }
+  }
+ * 
+ */
--- src/middleware-stack/roleCheck.ts
+++ src/middleware-stack/roleCheck.ts
@@ -1,16 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
-
-export default function roleCheck(req: NextRequest) {
+//1. เข้าถึง role ได้แล้ว
+//2. เช็คว่าการส่งข้อมูล user เกิดขึ้นตอนไหนบ้าง + useEffect ทำงานยังไงกันแน่
+//3. เขียน logic การตรวจสอบ role
+export default async function roleCheck(req: NextRequest) {
   console.log("From rolecheck");
-  const user = req.headers.get("userPayload");
+  //const user = req.headers.get("userPayload");
+  const user = req.headers.get("userRole");
+  console.log("Role check. 1");
+
   if (user) {
-    const payload = JSON.parse(user);
-    console.log(`Payload from role check.`);
-    console.log(payload);
-    console.log(`________________________`);
+    console.log("Role check. 2.5");
+    const role = JSON.parse(user);
+    console.log(role.user_metadata.role);
+    const userRole = role.user_metadata.role;
+    req.headers.set(`userRole`, JSON.stringify(userRole));
   } else {
-    console.log("Role: Unauthenticated");
+    console.log("Role check. 2");
+    const userRole = "guest";
+    req.headers.set(`userRole`, JSON.stringify(userRole));
   }
 
+  console.log("Role check. 3");
+  console.log("Role: ");
+  console.log(req.headers.get(`userRole`));
+  console.log("Role check. 4");
+  console.log(`________________________`);
   return NextResponse.next();
 }
+//PASS
