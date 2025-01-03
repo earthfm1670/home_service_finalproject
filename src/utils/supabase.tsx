495dd8a85fe2b4b322a880c839bc6c8e7a2081f0
Thanapat1502 <psy150201027@gmail.com>
2025-01-03 10:19:27 +0700

    feature: create new supabase clien for cookies system

--- src/utils/supabase.tsx
+++ src/utils/supabase.tsx
@@ -1,4 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+//-------------------------------------------------------------------
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const admininKey = process.env.SUPABASE_SERVICE_ROLE;
@@ -13,17 +16,64 @@ if (!admininKey) {
   throw new Error("Admin Key is missing");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
-export const adminSupabase = createClient(supabaseUrl, admininKey, {
-  auth: {
-    persistSession: false,
-    autoRefreshToken: false,
-    detectSessionInUrl: false,
-  },
-});
+async function createSupabaseClient() {
+  const cookieStore = await cookies();
 
-//TODO Upadate all admin api using adminSupabase
-//TODO admin register: add meta data
-//TODO admin login: change into adminSupabase client
-//TODO user register: add meta data
-//TODO check if nessessary to updatre CRUD for admin api
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            );
+          } catch (e) {
+            const error = e as Error;
+            console.log(`set cookies error: `, error.message);
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    }
+  );
+}
+
+async function createAdminClient() {
+  const cookieStore = await cookies();
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            );
+          } catch (e) {
+            const error = e as Error;
+            console.log(`set cookies error: `, error.message);
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    }
+  );
+}
+//-------------------------------------------------------------
+
+export const supabase = createSupabaseClient();
+export const adminSupabase = createAdminClient();
