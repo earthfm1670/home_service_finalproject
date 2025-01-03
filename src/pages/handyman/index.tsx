d83e79f0785bf5b64b22e1672e78730f6fc66b75
Kittisak Saengsri <kittisaksa77@gmail.com>
2025-01-03 09:36:13 +0700

    feat: add handyman page and styling

---
+++ src/pages/handyman/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "@/components/handyman/main";
+
+const Handyman: React.FC = () => {
+  return (
+    <>
+      <Main />
+    </>
+  );
+};
+
+export default Handyman;
