e4e2f23301e3227ab60b35df25c168567757b190
Thanapat1502 <psy150201027@gmail.com>
2025-01-06 21:10:58 +0700

    feature: create discount api for customer to use discount code on payment process, adjust database for usage counter

---
+++ src/pages/api/discount/index.ts
@@ -0,0 +1,54 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { supabase } from "@/utils/supabase";
+
+export default async function useDiscountCode(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { promotionCode, useCount } = req.body;
+  // for test only
+  if (useCount) {
+    console.log(useCount);
+  }
+  // 1. validate promotion code: valid and use count remain
+  const { data: hasPromotion, error: hasPromotionError } = await supabase
+    .from("promotion_codes")
+    .select(`promotion_id, discount_value, promotion_status`)
+    .eq(`promotion_code`, promotionCode)
+    .single();
+
+  if (hasPromotionError) {
+    console.log(hasPromotionError.message);
+    return res.status(404).json({
+      message: `Promotion code not found: `,
+      error: hasPromotionError.message,
+    });
+  }
+
+  if (hasPromotion.promotion_status === "unavailable") {
+    return res
+      .status(400)
+      .json({ error: "Promotion code is no longer available." });
+  }
+  // 2. use code by -1 out of useage pool
+  if (hasPromotion) {
+    const { error: useCodeFail } = await supabase.rpc(
+      "update_promotion_use_count",
+      {
+        input_promotion: promotionCode,
+        use_count: useCount,
+      }
+    );
+    // 2.5 handle fail scinario
+    if (useCodeFail) {
+      console.log(`Fail to use promotion code: ${useCodeFail.message}`);
+      return res
+        .status(400)
+        .json({ message: "Fail to use code", error: useCodeFail.message });
+    }
+  }
+  // // 3. return discount value
+  return res
+    .status(201)
+    .json({ message: `Use promotion code successfully.`, data: hasPromotion });
+}
