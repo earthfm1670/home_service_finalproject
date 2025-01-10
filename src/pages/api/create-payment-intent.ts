c6e0ad433a0c403e53ddc6d45c7d136da8095835
Pumthad Kaewmongkolsri <pumthad1670@gmail.com>
2025-01-10 08:35:05 +0700

    fix: suppress create-payment-intent api type error

--- src/pages/api/create-payment-intent.ts
+++ src/pages/api/create-payment-intent.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2020-08-27",
+  apiVersion: "2020-08-27" as any,
 });
 
 const promoCodes: { [key: string]: number } = {
